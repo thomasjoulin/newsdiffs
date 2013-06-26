78325f198a8b817a88f365b2b7574d1e3004fdf5
Morganko <morgan.korchia@gmail.com>
2013-06-26 16:34:18 +0000

    correct article /divers/

--- parsers/lefigaro.py
+++ parsers/lefigaro.py
@@ -5,13 +5,23 @@ from datetime import datetime
 DATE_FORMAT = '%B %d, %Y at %l:%M%P EDT'
 
 class LeFigaroParser(BaseParser):
-    domains = ['www.lefgiaro.fr']
+    domains = ['www.lefigaro.fr']
 
     feeder_pages = ['http://www.lefigaro.fr/']
     feeder_pat  = '^http://www.lefigaro.fr/[^/photos][a-zA-Z-]+/\d{4}/\d{2}/\d{2}/[a-zA-Z0-9-]+.php$'
 
     def _parse(self, html):
         soup = BeautifulSoup(html, convertEntities=BeautifulSoup.HTML_ENTITIES, fromEncoding='utf-8')
+
+        try:
+            p_tags = soup.find('div', attrs={'itemprop':'articleBody'}).findAll('p')
+        except AttributeError:
+            self.real_article = False
+            return
+
+        main_body = '\n'.join([p.getText() for p in p_tags])
+
+        self.body = main_body
 
         self.meta = soup.findAll('meta')
 
@@ -29,8 +39,5 @@ class LeFigaroParser(BaseParser):
         datet = datetime.strptime(new_dt, '%Y-%m-%dT%H:%M:%S')
         self.date = datet.strftime(DATE_FORMAT)
         
-        p_tags = soup.find('div', attrs={'itemprop':'articleBody'}).findAll('p')
-        main_body = '\n'.join([p.getText() for p in p_tags])
-        
-        self.body = main_body
+
 
